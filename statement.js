9e92c3e5a5ecc867397588444d48d81f754737b5
tyler <baile320@umn.edu>
2021-07-14 07:55:23 -0500

    replace temp with query - playFor

--- statement.js
+++ statement.js
@@ -3,6 +3,9 @@ module.exports = {
 }
 
 function statement(invoice, plays) {
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
     function amountFor(aPerformance, play) {
         let result = 0;
         switch (play.type) {
@@ -33,7 +36,7 @@ function statement(invoice, plays) {
             minimumFractionDigits: 2
         }).format;
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
+        const play = playFor(perf);
         let thisAmount = amountFor(perf, play);
 
         // add volume credits
--- tests.js
+++ tests.js
@@ -6,3 +6,4 @@ const { statement } = require('./statement.js');
 const correct_output = `Statement for BigCo\n\tHamlet: $650.00 (55 seats)\n\tAs You Like It: $580.00 (35 seats)\n\tOthello: $500.00 (40 seats)\n\tAmount owed is $1,730.00\nYou earned 47 credits\n`
 let test_output = statement(invoices[0], plays)
 console.log("Does the output match?", test_output == correct_output);
+console.log(test_output);
